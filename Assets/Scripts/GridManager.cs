c94ca525a60e91ee174eaf54c0249eab4d8bf2c2
Pol Blesa <pol.blesa@enti.cat>
2025-02-27 00:03:51 +0100

    Level9 & Level10 reformados

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -287,42 +287,31 @@ public class GridManager : MonoBehaviour
                 break;
             case "Level_9":
                 levelColors = new Color[]
-                {
-                    Color.red, Color.red, Color.red, Color.red, Color.red, Color.blue, Color.blue, Color.blue, Color.blue, Color.blue,
-                    Color.magenta, Color.magenta, Color.magenta, Color.magenta, Color.magenta, Color.yellow, Color.yellow, Color.red, Color.red, Color.red,
-                    Color.magenta, Color.blue, Color.blue, Color.blue, Color.blue, Color.magenta, Color.magenta, Color.magenta, Color.yellow, Color.yellow,
-                    Color.magenta, Color.yellow, Color.magenta, Color.magenta, Color.magenta, Color.magenta, Color.yellow, Color.yellow, Color.yellow, Color.yellow,
-                    Color.yellow, Color.yellow, Color.yellow, Color.yellow, Color.magenta, Color.magenta, Color.magenta, Color.magenta, Color.magenta, Color.magenta,
-                    Color.yellow, Color.yellow, Color.magenta, Color.magenta, Color.magenta, Color.blue, Color.blue, Color.blue, Color.blue, Color.magenta,
-                    Color.red, Color.red, Color.red, Color.yellow, Color.yellow, Color.magenta, Color.magenta, Color.magenta, Color.magenta, Color.magenta,
-                    Color.blue, Color.blue, Color.blue, Color.blue, Color.blue, Color.yellow, Color.yellow, Color.yellow, Color.yellow, Color.yellow
-                };
+                 {
+                    Color.yellow, Color.magenta, Color.yellow, Color.magenta, Color.magenta, Color.magenta, Color.red,
+                    Color.yellow, Color.magenta, Color.yellow, Color.yellow, Color.blue, Color.magenta, Color.red,
+                    Color.yellow, Color.magenta, Color.yellow, Color.magenta, Color.blue, Color.magenta, Color.red,
+                    Color.yellow, Color.magenta, Color.yellow, Color.magenta, Color.blue, Color.magenta, Color.red,
+                    Color.blue, Color.yellow, Color.magenta, Color.magenta, Color.blue, Color.magenta, Color.blue,
+                    Color.blue, Color.yellow, Color.yellow, Color.magenta, Color.magenta, Color.magenta, Color.blue,
+                    Color.blue, Color.red, Color.yellow, Color.magenta, Color.blue, Color.magenta, Color.blue,
+                    Color.blue, Color.red, Color.yellow, Color.yellow, Color.blue, Color.magenta, Color.blue,
+                    Color.blue, Color.red, Color.yellow, Color.magenta, Color.magenta, Color.magenta, Color.blue,
+                 };
                 break;
             case "Level_10":
                 levelColors = new Color[]
-                {
-                    Color.green, Color.red, Color.green, Color.green, Color.blue, Color.blue, Color.yellow, Color.yellow, Color.green, Color.green,
-                    Color.red, Color.red, Color.green, Color.blue, Color.blue, Color.yellow, Color.yellow, Color.red, Color.red, Color.green,
-                    Color.green, Color.green, Color.blue, Color.blue, Color.red, Color.red, Color.yellow, Color.yellow, Color.blue, Color.green,
-                    Color.green, Color.blue, Color.blue, Color.red, Color.red, Color.yellow, Color.yellow, Color.blue, Color.blue, Color.blue,
-                    Color.blue, Color.blue, Color.red, Color.red, Color.yellow, Color.yellow, Color.green, Color.blue, Color.red, Color.red,
-                    Color.blue, Color.red, Color.red, Color.yellow, Color.yellow, Color.green, Color.green, Color.blue, Color.red, Color.red,
-                    Color.red, Color.red, Color.yellow, Color.yellow, Color.green, Color.green, Color.blue, Color.blue, Color.yellow, Color.yellow,
-                    Color.red, Color.yellow, Color.yellow, Color.green, Color.green, Color.blue, Color.blue, Color.red, Color.yellow, Color.yellow
-                };
-                break;
-            case "Level_11":
-                levelColors = new Color[]
-                {
-                    Color.red, Color.red, Color.red, Color.yellow, Color.red, Color.red, Color.blue, Color.blue, Color.blue, Color.red,
-                    Color.red, Color.green, Color.yellow, Color.yellow, Color.red, Color.yellow, Color.yellow, Color.blue, Color.blue, Color.red,
-                    Color.red, Color.green, Color.blue, Color.green, Color.red, Color.blue, Color.blue, Color.red, Color.red, Color.red,
-                    Color.green, Color.green, Color.blue, Color.green, Color.red, Color.red, Color.blue, Color.yellow, Color.yellow, Color.yellow,
-                    Color.blue, Color.blue, Color.blue, Color.green, Color.green, Color.green, Color.blue, Color.red, Color.red, Color.yellow,
-                    Color.blue, Color.yellow, Color.blue, Color.blue, Color.green, Color.green, Color.green, Color.green, Color.yellow, Color.yellow,
-                    Color.blue, Color.red, Color.red, Color.blue, Color.blue, Color.blue, Color.blue, Color.green, Color.yellow, Color.blue,
-                    Color.blue, Color.red, Color.yellow, Color.yellow, Color.green, Color.green, Color.green, Color.green, Color.green, Color.blue
-                };
+                 {
+                    Color.green, Color.yellow, Color.yellow, Color.blue, Color.green, Color.red, Color.green,
+                    Color.green, Color.red, Color.yellow, Color.blue, Color.green, Color.red, Color.red,
+                    Color.blue, Color.blue, Color.yellow, Color.blue, Color.blue, Color.green, Color.green,
+                    Color.blue, Color.green, Color.yellow, Color.yellow, Color.blue, Color.blue, Color.blue,
+                    Color.blue, Color.green, Color.green, Color.yellow, Color.red, Color.red, Color.blue,
+                    Color.blue, Color.blue, Color.green, Color.yellow, Color.yellow, Color.red, Color.red,
+                    Color.red, Color.blue, Color.green, Color.green, Color.yellow, Color.yellow, Color.red,
+                    Color.yellow, Color.blue, Color.blue, Color.green, Color.green, Color.yellow, Color.red,
+                    Color.yellow, Color.red, Color.blue, Color.blue, Color.green, Color.yellow, Color.yellow,
+                 };
                 break;
             default:
                 Debug.LogError("Nivel no definido.");
